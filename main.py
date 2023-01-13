54077b9bc0e57e748151404546702dec2dea4dd1
Kyle Kanaly <54219067+kjtkanaly@users.noreply.github.com>
2023-01-13 11:23:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -15,8 +15,9 @@ def main():
     fs = 20
     ts = 1 / fs
     T = 4
+    amplitude = 1.2
     freqStep = 0.01
-    frameRate = 60
+    frameRate = 120
     frameDelay = 1 / frameRate
 
     # Execution
@@ -27,26 +28,31 @@ def main():
                             stop=(fs - freqStep), 
                             num=(int(fs / freqStep)))
 
-    signal = np.sin(2 * math.pi * time * freqSweep[0])
+    # signal = np.sin(2 * math.pi * time * freqSweep[0])
+    signal = np.sin(2 * math.pi * time * 2)
 
     fig, ax = plt.subplots()
-    ax.set_ylim([-1, 1])
+    ax.set_ylim([-amplitude, amplitude])
     ax.set_xlim([0, T])
     
-    line, = ax.plot(time, signal, label=str(freqSweep[0]))
+    line, = ax.plot(time, signal, label=str(freqSweep[0]), zorder=10)
     line.set_marker("o")
-    line.set_color('w')
+    line.set_color('b')
     line.set_markeredgecolor('b')
 
-    leg = ax.legend()
+    leg = ax.legend(loc=1)
     leg.set_title("Frequency")
+    leg.set_zorder(20)
 
     def animate(i):
         print(len(leg.get_texts()))
+
         signal = np.sin(2 * math.pi * time * freqSweep[i])
         line.set_ydata(signal)
-        leg.get_texts()[0].set_text("test")
-        return line,
+        leg.get_texts()[0].set_text(str(round(freqSweep[i], 2)))
+        leg.set_zorder(20)
+
+        return line, leg,
 
     anime = animation.FuncAnimation(
             fig, animate, interval=frameDelay*1000, blit=True, save_count=50)
